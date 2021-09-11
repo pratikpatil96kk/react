398888ce4cf642358b94652da6cd3a8067216e8f
pratik <pratikshinde1109@gmail.com>
2021-09-11 12:46:14 +0530

    uploding

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Demo from './demo'
+
+const name = 'pratik' ;
+
+function App() {
+  return (
+    <div className="App">
+      <Demo/>
+      <Demo></Demo>
+       <h1>Hello my name is {name} </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/demo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const name = 'pratik'
+
+const demo = () => {
+    return (
+        <div>
+            my name is {name} 
+        </div>
+    )
+}
+
+export default demo
